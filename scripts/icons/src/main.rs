dc1cedd922d1de3e9a10f29648dbbc3e481e2060
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-02 00:34:38 +0000

    fixes

--- scripts/icons/src/main.rs
+++ scripts/icons/src/main.rs
@@ -1,8 +1,14 @@
-const SIZES: [u8; 6] = [16, 32, 48, 64, 96, 128];
+use image::imageops::FilterType;
+
+const SIZES: [u32; 6] = [16, 32, 48, 64, 96, 128];
 
 fn main() -> anyhow::Result<()> {
     let img = image::open("./icon.png")?;
-    println!("Hello, world!");
+
+    for size in SIZES {
+        let img = img.clone();
+        img.resize(size, size, FilterType::Nearest);
+    }
 
     Ok(())
 }
