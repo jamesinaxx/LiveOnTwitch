6c11cdf571f6abd81af67af463b1631cdac1c5bb
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-02 00:32:23 +0000

    added anyhow and read image

--- scripts/icons/src/main.rs
+++ scripts/icons/src/main.rs
@@ -1,5 +1,8 @@
 const SIZES: [u8; 6] = [16, 32, 48, 64, 96, 128];
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    let img = image::open("./icon.png")?;
     println!("Hello, world!");
+
+    Ok(())
 }
