909563b04b822fed07bcbaba6d3a1858f979d02e
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-02 00:47:16 +0000

    moved to rust for creating icons and added them to gitignore

--- scripts/icons/src/main.rs
+++ scripts/icons/src/main.rs
@@ -1,14 +1,26 @@
-use image::imageops::FilterType;
+use image::{imageops::FilterType, ImageFormat};
+use rayon::prelude::*;
 
 const SIZES: [u32; 6] = [16, 32, 48, 64, 96, 128];
 
 fn main() -> anyhow::Result<()> {
     let img = image::open("./icon.png")?;
+    let dest_path = std::env::current_dir()?.join("../../src/assets/icons");
 
-    for size in SIZES {
+    std::fs::create_dir_all(&dest_path)?;
+
+    SIZES.into_par_iter().for_each(|size| {
         let img = img.clone();
-        img.resize(size, size, FilterType::Nearest);
-    }
+        let resized = img.resize(size, size, FilterType::Nearest);
+
+        let image_name = format!("{}.png", size);
+        let image_path = dest_path.join(image_name);
+        let res = resized.save_with_format(image_path, ImageFormat::Png);
+
+        if let Err(err) = res {
+            eprintln!("Error resizing icon {}: {}", size, err);
+        }
+    });
 
     Ok(())
 }
