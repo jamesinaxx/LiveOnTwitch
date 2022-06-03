c9b101f2ebd002e2c2c22a04c3c1f7158e4b2045
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-06-03 01:09:59 +0000

    added basic implementation for reading env files

--- env/src/lib.rs
+++ env/src/lib.rs
@@ -1,10 +1,40 @@
+use wasm_bindgen::prelude::*;
+
 const ENV_FILE: &str = include_str!("../../.env");
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+pub struct EnvFile {
+    pub client_id: &'static str,
+    pub client_secret: &'static str,
+}
+
+#[wasm_bindgen]
+impl EnvFile {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut client_id = "";
+        let mut client_secret = "";
+
+        for line in ENV_FILE.lines() {
+            let parts: Vec<&str> = line.split('=').collect();
+            if parts.len() == 2 {
+                if parts[0] == "CLIENT_ID" {
+                    client_id = parts[1];
+                } else if parts[0] == "CLIENT_SECRET" {
+                    client_secret = parts[1];
+                }
+            }
+        }
+
+        Self {
+            client_id,
+            client_secret,
+        }
+    }
+}
+
+impl Default for EnvFile {
+    fn default() -> Self {
+        Self::new()
     }
 }
